6e0eaa4afed54cb23ea08fa9595a8d5082695cc6
Ivaan <ivaan@nazaroff.ca>
2023-09-30 21:42:38 -0700

    work in progress (testing git push)

--- durakGame/gameState.go
+++ durakGame/gameState.go
@@ -1,12 +1,16 @@
 package durakGame
 
 import (
-	"durak/durakRules"
+	"github.com/Ivaan/durak/durakRules"
 )
 
 type GameState struct {
-	durakRules.PlayState
+	PlayerA  durakRules.Hand
+	PlayerB  durakRules.Hand
+	DrawDeck []int //bit possition of card, yes this is a tighter coupling than I imagined .. this is fine
 	SuitOrder
+	PlayerBAttacker   bool //otherwise PlayerA is PlayerBAttacker
+	DefenderDefending bool //otherwise PlayerBAttacker attacking
 }
 
 type SuitOrder [4]Suit
